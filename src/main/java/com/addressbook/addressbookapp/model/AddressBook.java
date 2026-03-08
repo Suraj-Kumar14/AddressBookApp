5b2d42f24b74a2097f386faca9c5fec7333e2839
Suraj Kumar <suraj1122003@gmail.com>
2026-03-08 22:28:18 +0530

    [Suraj]: Added searchByState method in AddressBook class

--- src/main/java/com/addressbook/addressbookapp/model/AddressBook.java
+++ src/main/java/com/addressbook/addressbookapp/model/AddressBook.java
@@ -100,4 +100,11 @@ public class AddressBook {
 			}
 		}).forEach(System.out::println);
 	}
+	
+	public void viewByState(String state) {
+		contactList
+			.stream()
+			.filter(x-> x.getState().equalsIgnoreCase(state)).forEach(System.out::println);
+	}
+
 }
