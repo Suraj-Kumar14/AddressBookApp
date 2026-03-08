c7b23287fb19f8f518fe25a942c68c89d4561b9a
Suraj Kumar <suraj1122003@gmail.com>
2026-03-08 14:21:33 +0530

    [Suraj]: Added AddressBookAppApplication class file

---
+++ src/main/java/com/addressbook/addressbookapp/AddressBookAppApplication.java
@@ -0,0 +1,15 @@
+package com.addressbook.addressbookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.addressbook.addressbookapp.model.Contact;
+
+@SpringBootApplication
+public class AddressBookAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookAppApplication.class, args);
+	}
+
+}
