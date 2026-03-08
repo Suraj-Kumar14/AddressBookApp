7e46ce920f9c64cd30c1cda0bd7c854d602b3776
Suraj Kumar <suraj1122003@gmail.com>
2026-03-08 19:27:29 +0530

    [Suraj]: Added AddressBookSystem class file

---
+++ src/main/java/com/addressbook/addressbookapp/model/AddressBookSystem.java
@@ -0,0 +1,26 @@
+package com.addressbook.addressbookapp.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class AddressBookSystem {
+	Map<String, AddressBook> addressbooksystem = new HashMap<>();
+	
+	public void addAddressBook(String name) {
+		if(addressbooksystem.containsKey(name)) {
+			System.out.println("Address book already exist!");
+			return;
+		}
+		
+		addressbooksystem.put(name, new AddressBook());
+	}
+	
+	public AddressBook getAddressBook(String name) {
+		return addressbooksystem.get(name);
+	}
+	
+	public Set<String> listAllAddressBooks() {
+		return addressbooksystem.keySet();
+	}
+}
