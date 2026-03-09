a04f347cd747cfa356113a66be1b590da6fd9baf
Suraj Kumar <suraj1122003@gmail.com>
2026-03-10 01:19:13 +0530

    [Suraj]: Added addMultipleEntries testcases in AddressBookJsonServerTest class

--- src/test/java/com/addressbook/addressbookapp/AddressBookJsonServerTest.java
+++ src/test/java/com/addressbook/addressbookapp/AddressBookJsonServerTest.java
@@ -1,5 +1,6 @@
 package com.addressbook.addressbookapp;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,10 +10,12 @@ import com.addressbook.addressbookapp.model.AddressBookService;
 import com.addressbook.addressbookapp.model.Contact;
 import static io.restassured.RestAssured.*;
 
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 public class AddressBookJsonServerTest {
 	AddressBookService service = new AddressBookService();
+	List<Contact> addressBookMemory = new ArrayList<>();
 	
 	@Test
     public void readContactsFromJsonServer() {
@@ -34,4 +37,29 @@ public class AddressBookJsonServerTest {
 
         service.getContacts().forEach(System.out::println);
     }
+	
+	@Test
+	public void addMultipleContacts() {
+		 List<Contact> newContacts = List.of(
+	                new Contact("Ravi","Kumar","addr1","Hyderabad","TS","500001","9991112222","ravi@gmail.com"),
+	                new Contact("Rahul","Sharma","addr2","Delhi","DL","110001","9992223333","rahul@gmail.com")
+	        );
+		 
+		 newContacts.forEach(contact->{
+			 Contact responseContact = 
+					 given()
+					 	.contentType(ContentType.JSON)
+					 	.body(contact)
+					 .when()
+					 	.post("http://localhost:3000/contacts")
+					 .then()
+					 	.statusCode(201)
+					 	.extract()
+					 	.as(Contact.class);
+			 addressBookMemory.add(responseContact);
+		 });
+		 
+		 System.out.println("Contact in memory");
+		 addressBookMemory.forEach(System.out::println);
+	}
 }
